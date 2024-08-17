78d53ffe4b4ce0cbd9ec10a080965182f38136b6
Mivel khansa <mivelkhansa6@gmail.com>
2024-08-17 13:07:48 +0700

    Add files via upload

---
+++ calculator3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main() {
+    //the variable
+    std::string operation;
+    double num1,num2;
+    //take the user input
+    std::cout << "enter a number: ";
+    std::cin >> num1;
+    
+    std::cout << "enter a number: ";
+    std::cin >> num2;
+    
+    std::cout << "enter a operation(+,-,*,/): ";
+    std::cin >> operation;
+    //if statement 
+    if(operation == "+"){
+        std::cout << num1 + num2;
+    }
+    
+    else if (operation == "-"){
+        std::cout << num1 - num2;
+    }
+    
+    else if (operation == "*"){
+        std::cout << num1 * num2;
+    }
+    
+    else if (operation == "/"){
+        std::cout << num1 / num2;
+    }
+    
+    else{
+        std::cout << "error";
+    }
+    return 0;
+}
